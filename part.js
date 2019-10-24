e668d42a189da4e4c4bdacf57c7f7464d884a493
sihui <me@sihuiliang.com>
2019-10-24 15:39:26 +1100

    Correct render set

--- part.js
+++ part.js
@@ -1,4 +1,4 @@
 import React from 'react';
 
-const list = [1,2,3,4,5,6,7,7,8,8]
-export const Part = <div>{list.map(i => <p> number {i} </p> )}</div>
+const list = [1,2,3,4,5,6,7,71,8,82]
+export const Part = () => <div>{list.map(i => <p key={i}> number {i} </p> )}</div>;
