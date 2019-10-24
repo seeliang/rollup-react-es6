a78a9ab41baf8677992898b8293338607ebe602f
sihui <me@sihuiliang.com>
2019-10-24 14:50:15 +1100

    Init

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Part } from './part';
+
+function App() {
+  return (
+    <div className="App">
+      <Part />
+    </div>
+  );
+}
+
+export default App;
---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/js/bundle.js',
+    format: 'esm'
+  }
+};
